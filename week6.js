6d15050cd13a5f8bba273b32a25d98e53734ebe0
Rey-Berkelmans <143152147+Rey-Berkelmans@users.noreply.github.com>
2023-09-17 12:07:48 +1000

    Update week6.js

--- week6.js
+++ week6.js
@@ -9,8 +9,12 @@ function setup() {
 
 function draw() {
   background(0,0,0);
+
+  // I tried to add code that would allow users to change the colour of background when a key was pressed. I was unable to get this to work without breaking the rest of the code so I've left the background just as plain back.
   
   if (mouseIsPressed) {
+
+    // Attached below is the code for the snow/circles
 
   for (let i = 0; i < circleY.length; i++) {
     let circleX = width * i / circleY.length;
@@ -32,6 +36,9 @@ function draw() {
     if (circleY[i] > height) {
       circleY[i] = 0;
     }
+
+  // Snowman code below
+    
    ellipse(200,400,450,100)
     ellipse(200,320,100,100)
   ellipse(200,260,80,80)
