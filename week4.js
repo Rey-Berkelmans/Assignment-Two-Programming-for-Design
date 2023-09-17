ec5ccb6015aa6cd8c7b3a50a557e13600141a9c5
Rey-Berkelmans <143152147+Rey-Berkelmans@users.noreply.github.com>
2023-09-17 11:34:31 +1000

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,3 +1,15 @@
-/*
-    Paste the code for your week 4 exercise below.
-*/
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0,0,255);
+  ellipse(200,400,450,100)
+    ellipse(200,320,100,100)
+  ellipse(200,260,80,80)
+  ellipse(200,200,60,60)
+  ellipse(185,200,10,10)
+  ellipse(215,200,10,10)
+  textSize(50);
+text('Snowman',80,100)
+}
